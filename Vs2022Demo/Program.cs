8647537071ed8e4daefe5fe3daa34f46504393b3
Satish Yadav <monstersatish@live.com>
2021-06-20 12:27:14 +0530

    Add project files.

---
+++ Vs2022Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Vs2022Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in args)
+            {
+                sb.AppendLine(item);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
